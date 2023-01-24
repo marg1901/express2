b04e8ec2a4ebfa35096baeab613beca3d7bf2ab6
margaux.begard <margaux.begard2@gmail.com>
2023-01-24 09:53:29 +0100

    Valider la saisie utilisateur

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 const app = express();
 const port = process.env.APP_PORT ?? 5000;
 const userMaster = require("./userMaster");
+const validateUser = require("./validateUser.js");
 
 app.get("/api/users", userMaster.getUsers);
 app.get("/api/users/:id", userMaster.getUsersById);
 app.post("/api/users", userMaster.postUsers);
 app.put("/api/users/:id,", userMaster.putUsers);
 app.delete("/api/users/:id", userMaster.deleteUsers);
+
+// Express4 BIS
+app.post("/api/users", validateUser.validateUser);
+app.put("/api/users/:id,", validateUser.validateUser);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ validateUser.js
@@ -0,0 +1,31 @@
+const validateUser = (req, res, next) => {
+  const { firstname, lastname, city, language } = req.body;
+  const { email } = req.body;
+  const errors = [];
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+  if (firstname == null) {
+    errors.push({ field: "firstname", message: "This field is required" });
+  }
+  if (lastname == null) {
+    errors.push({ field: "lastname", message: "This field is required" });
+  }
+  if (city == null) {
+    errors.push({ field: "city", message: "This field is required" });
+  }
+  if (language == null) {
+    errors.push({ field: "language", message: "This field is required" });
+  }
+  if (!emailRegex.test(email)) {
+    errors.push({ field: "email", message: "Invalid email" });
+  }
+  if (errors.length) {
+    res.status(422).json({ validationErrors: errors });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  validateUser,
+};
